429579561bed4e652c68f8c3b74dd9d80e1bb723
pallavi-gatade <pallavi.gatade24@gmail.com>
2024-11-26 18:50:35 +0530

    Add files via upload
    ArrayList programs coding practice 26th nov

---
+++ ArraylistPractice.java
@@ -0,0 +1,24 @@
+package collection;
+import java.util.*;
+import java.util.List;
+
+public class ArraylistPractice {
+	
+//	Write a Java program to create an array list, add some colors (strings) and print out the collection.
+
+	public static void main(String[] args) {
+
+//		List<String> colors = new ArrayList();
+		
+		List<String> colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("white");
+        colors.add("yellow");
+        colors.add("blue");
+        colors.add("purple");
+        colors.add("pink");
+        System.out.println(colors);
+		
+	}
+
+}
